ebfb329b2592cf4e8a7e6e2f6835da3a28e0bfa2
Yeasin <yeasin30112001@gmail.com>
2021-03-06 23:23:46 -0600

    some components created for pactice

---
+++ src/components/CategoryDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CategoryDetails = () => {
+    return (
+        <div>
+            <h3>This is category details</h3>
+        </div>
+    );
+};
+
+export default CategoryDetails;
